76ac70b89700bd6f1f3cffedd124589cceeb13fb
Ben Zeng <benjaminzeng@gmail.com>
2019-04-30 10:34:25 -0700

    this is the updated version, worked on playerlink, need keylistener

--- Zelduh/src/PlayerLink.java
+++ Zelduh/src/PlayerLink.java
@@ -1,9 +1,15 @@
 
+
 public class PlayerLink {
 	//i need to write new code and im just checking if this works
 	//this the big gae
 	private Image i;
+	private int g
 	public PlayerLink(){
+		this.i = src/;
+	}
+	
+	public Image changeImage() {
 		
 	}
 }
