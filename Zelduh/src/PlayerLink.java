dfc98d59bfa7e0a197fc5555f9ccbae592963cc2
Ryan <ryan.rajesh@gmail.com>
2019-04-30 11:06:20 -0700

    Committed?

--- Zelduh/src/PlayerLink.java
+++ Zelduh/src/PlayerLink.java
@@ -3,6 +3,7 @@
 public class PlayerLink {
 	//i need to write new code and im just checking if this works
 	//this the big gae
+	//Is this commit
 	private Image i;
 	private int g
 	private Location start;
